c6eca757a13b82ed83f89d76e9182c352a3a5155
ares <perzynskiwojciech@gmail.com>
2024-09-30 20:10:33 +0200

    Calculate the area of rectangle. The user provides the side lengths.

---
+++ Task-07-rectangle-area.py
@@ -0,0 +1,10 @@
+#Calculate the area of a rectangle. The user provides the side lengths.
+
+area = 0
+
+Aside = int(input("Provide the length of side a: ")) # side a provide by the user
+Bside = int(input("Provide the length of side b: ")) # side b provide by the user
+
+area = Aside * Bside  # rectangle area
+
+print(f"Rectangle area is {area}")
