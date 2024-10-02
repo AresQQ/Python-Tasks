9d75b49497276c9fcf9d081fa05fcfe8b9e45cb7
ares <perzynskiwojciech@gmail.com>
2024-10-02 15:26:11 +0200

    Guess number game

---
+++ Task-010-TheGame.py
@@ -0,0 +1,21 @@
+# Create a game in which a number between 1 and 100 is randomly selected and stored in a variable. Then, ask the user to guess this number.
+# After each attempt, display information indicating whether the user's guess is smaller or larger than the randomly selected number.
+# When the user guesses the number, end the game.
+
+import random
+
+#Generate random number between 1 and 100
+random_number = random.randint(1,100)
+user_number = None
+
+while True:  # Loop runs until met break condition.
+    user_number = int(input("Podaj liczbę: "))
+
+    #Check if the user's number is smaller than the random number and return hint or end the loop on right guess.
+    if user_number < random_number:
+        print("Twoja liczba jest mniejsza od naszej spróbuj jeszce raz")
+    elif user_number > random_number:
+        print("Twoja liczba jest wiekszą od naszej")
+    else:
+        print("Brawo zgadłeś!")
+        break
